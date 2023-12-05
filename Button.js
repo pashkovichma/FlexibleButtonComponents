831993ce59d0fe099ee2562da68a269b67255809
Maria <pashkovichma@gmail.com>
2023-12-05 09:41:56 +0100

    buttons

---
+++ Button.js
@@ -0,0 +1,29 @@
+export default function Button({children, mode = 'filled', Icon, ...props}) {
+  let cssClasses = `button ${mode}-button`;
+  
+  if (Icon) {
+      cssClasses += ' icon-button';
+  }
+  
+  if (props.className) {
+      cssClasses += ' ' + props.className;
+  }
+ 
+  
+  return (
+      <button className={cssClasses} {...props}>
+      {Icon && (
+          <span className="button-icon">
+              <Icon />
+          </span>
+      )}
+      <span>{children}</span>
+  </button>
+  );
+  
+// Todo: Build this component!
+
+// !!! Important: 
+// Wrap the icon with a <span className="button-icon"> to achieve the target look
+}
+
